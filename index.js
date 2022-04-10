e4c894f8ee82e479d3c7dbdece13b68e263dfee9
jiteshcb1 <jitesh.chhabra@outlook.com>
2022-04-10 16:50:14 +0530

    Update related to random values

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let isAlive = true;
-let message = "";
-
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-//let sumEl = document.querySelector("#sum-el"); // works as same as getElementById but it is more dynamic as we need to specify
-//which type of element like ID, Tag or class instead... to selct css classes we use "." DOT infront of the elemnt
+
+let player = {
+    name : "JC",
+    chips : 1450
+}
+
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + " : ₹" + player.chips
+function getRandomCard(){
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if(randomNumber > 10){
+        return 10
+    }else if(randomNumber === 1){
+        return 11
+    }else {
+        return randomNumber
+    }
+}
 
 function startGame(){
-    cardsEl.textContent = "Cards : " + firstCard + " " + secondCard
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards : "
+
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum : " + sum
     if(sum <= 20){
         message = "Do you want to draw a new card? "
-    }else if(sum = 21){
+    }else if(sum === 21){
         message = "Wohoo! You've got Blackjack! "
         hasBlackJack = true;
-    }
-    else{
+    }else{
         message = "You're out of the game! "
         isAlive = false;
     }
@@ -28,4 +57,13 @@ function startGame(){
     messageEl.textContent = message
 }
 
+
+function newCard(){
+    if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card;
+        cards.push(card)
+        renderGame()
+    }
+}
 
